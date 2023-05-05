2ce6aaae25845912b7e78197189eba5b9cb184a8
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-05-05 18:37:23 +0900

    fix: change eslint setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,15 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {  "project": "./tsconfig.json", ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    "react/function-component-definition": "error",
+    "react/react-in-jsx-scope": "off"
   },
 }
