d9a4c0ca316fc07904fc716d542245adaae8c6f6
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-05-10 13:47:29 +0900

    feat: navigator animation

--- src/components/Works/WorkNavigator/WorkNavigator.tsx
+++ src/components/Works/WorkNavigator/WorkNavigator.tsx
@@ -1,4 +1,24 @@
 import styles from "./WorkNavigator.module.scss";
+import useWaffleScroll from "../../../libs/WaffleScroll/index";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
 export default function WorkNavigator() {
-  return <div className={styles.WorkNavigator}>Works</div>;
+  const { ref, scrollState } = useWaffleScroll<{ open: boolean }>(
+    ({ toggleState }) => {
+      toggleState(2, 3.1, "open");
+    },
+    { open: false },
+  );
+
+  return (
+    <div
+      className={cx("WorkNavigator", { close: !scrollState.open })}
+      ref={ref}
+    >
+      <div className={styles.static}>WORKS</div>
+      <div className={styles.header}>WORKS</div>
+    </div>
+  );
 }
